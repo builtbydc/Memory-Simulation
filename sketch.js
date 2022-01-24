7047749988febcc26f8b8a2a3ad3719195bf6365
Daniel Carlson <61763891+builtbydc@users.noreply.github.com>
2022-01-24 17:36:30 -0500

    commit for meeting

--- sketch.js
+++ sketch.js
@@ -1,85 +1,260 @@
 let DIMENSION;
-let X_OFFSET;
-let Y_OFFSET;
+let X_OFFSET_STORE;
+let Y_OFFSET_STORE;
 
-let DENSITY;
-let UNIT;
+let DENSITY_STORE;
+let UNIT_STORE;
+
+let X_OFFSET_MEM;
+let Y_OFFSET_DRAM;
+let Y_OFFSET_PMEM;
+
+let DENSITY_MEM;
+let UNIT_MEM;
+
+let HEIGHT_DRAM;
+let HEIGHT_PMEM;
 
 function create2DArray(rows) {
 	let arr = [];
-	for (var i = 0; i < rows; i++) {
+	for (let i = 0; i < rows; i++) {
 		arr[i] = [];
 	}
 	return arr;
 }
 
-function contain(pos) {
-	while (pos < 0)
-		pos += DIMENSION;
-	while (pos >= DIMENSION)
-		pos -= DIMENSION;
-	return pos;
+function xpos_store(x) {
+	return x * UNIT_STORE + X_OFFSET_STORE;
 }
 
-function xpos(x) {
-	return contain(x * UNIT) + X_OFFSET;
+function ypos_store(y) {
+	return y * UNIT_STORE + Y_OFFSET_STORE;
 }
 
-function ypos(y) {
-	return contain(y * UNIT) + Y_OFFSET;
+function xpos_mem(x) {
+	return x * UNIT_MEM + X_OFFSET_MEM;
+}
+function ypos_dram(y) {
+	return y * UNIT_MEM + Y_OFFSET_DRAM;
+}
+function ypos_pmem(y) {
+	return y * UNIT_MEM + Y_OFFSET_PMEM;
 }
 
-function span(s) {
-	return s * UNIT;
+function span_store(s) {
+	return s * UNIT_STORE;
 }
 
-let files;
+function span_mem(s) {
+	return s * UNIT_MEM;
+}
+
+function setConstants() {
+	DIMENSION = 0.4 * float(width);
+	X_OFFSET_STORE = DIMENSION / 8;
+	Y_OFFSET_STORE = (float(height) - DIMENSION) / 2;
+
+	DENSITY_STORE = int(pow(2, 7));
+	UNIT_STORE = DIMENSION / float(DENSITY_STORE);
+
+	HEIGHT_DRAM = 15;
+	HEIGHT_PMEM = 40;
+
+	DENSITY_MEM = int(75);
+	UNIT_MEM = DIMENSION / float(DENSITY_MEM);
+
+	X_OFFSET_MEM = 11 * X_OFFSET_STORE;
+	Y_OFFSET_DRAM = Y_OFFSET_STORE;
+	Y_OFFSET_PMEM = Y_OFFSET_DRAM + DIMENSION - HEIGHT_PMEM*UNIT_MEM;
+}
+
+let fileStructure;
+let fileList;
+
+let DRAMindex = 0;
+let DRAM;
+let heatDRAM = [];
+
+let PMEMindex = 0; 
+let PMEM;
+let heatPMEM = [];
+
+let swap;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight)
-	DIMENSION = 0.8 * min(float(width), float(height));
-	X_OFFSET = (float(width) - DIMENSION) / 2;
-	Y_OFFSET = (float(height) - DIMENSION) / 2;
+	setConstants();
 
-	DENSITY = int(pow(2, 7));
-	UNIT = DIMENSION / float(DENSITY);
+	fileStructure = createFileStructure();
+	fileList = fileAnalysis(fileStructure);
 
-	files = createFileStructure();
+	DRAM = create2DArray(HEIGHT_DRAM * DENSITY_MEM);
+	PMEM = create2DArray(HEIGHT_PMEM * DENSITY_MEM);
+
+	swap = create2DArray(HEIGHT_DRAM * DENSITY_MEM);
+	for(let i = 0; i < HEIGHT_DRAM * DENSITY_MEM; i++) {
+		swap[i][0] = -1;
+	}
+
+	background(255);
 
 	stroke(255);
 	fill(0);
-	background(255);
+	drawFileStructure();
 
-	noLoop();
 }
 
+let interval = 60;
+let time = 0;
+
 function draw() {
-	for (let i = 0; i < DENSITY; i++) {
-		for (let j = 0; j < DENSITY; j++) {
-			if (files[i][j] === -1) continue;
-			rect(xpos(j), ypos(i), span(files[i][j]), span(files[i][j]));
+	fill(0, 0, 0, 30);
+	stroke(255);
+	drawFileStructure();
+	
+	if(time == interval) {
+		scanMem();
+		time = 0;
+	}
+
+	fill(255);
+	noStroke();
+	rect(width / 2, 0, width / 2, height);
+
+	stroke(255);
+	for(let i = 0; i < DRAMindex; i++) {
+		if(heatDRAM[i] > 0) fill(0, 255, 0);
+		else if(heatDRAM[i] < 0) fill(255, 0, 0);
+		else fill(0);
+		rect(xpos_mem(i % DENSITY_MEM), ypos_dram(int(i / DENSITY_MEM)), span_mem(1), span_mem(1));
+	}
+	for(let i = 0; i < PMEMindex; i++) {
+		if(heatPMEM[i] > 0) fill(0, 255, 0);
+		else if(heatPMEM[i] < 0) fill(255, 0, 0);
+		else fill(0);
+		rect(xpos_mem(i % DENSITY_MEM), ypos_pmem(int(i / DENSITY_MEM)), span_mem(1), span_mem(1));
+	}
+
+	fill(0);
+	for(let i = 0; i < DRAMindex; i++) {
+		if(swap[i][0] != -1) {
+			rect(swap[i][0] += swap[i][4], swap[i][1] += swap[i][5], span_mem(1), span_mem(1));
+			if(abs(swap[i][0] - swap[i][2]) < 1) swap[i][0] = -1;
+		}
+	}
+	
+	requestPages();
+
+	time++;
+}
+
+function scanMem() {
+	for(let i = 0; i < DRAMindex; i++) {
+		if(heatDRAM[i] > 1) heatDRAM[i] = 1;
+		else if(heatDRAM[i] > -1) heatDRAM[i]--;
+	}
+	for(let i = 0; i < PMEMindex; i++) {
+		if(heatPMEM[i] > 1) {
+			let file = PMEM[i][0];
+			let page = PMEM[i][1];
+			let loc = fileList[file].pages[page].location;
+
+			for(let j = 0; j < DRAMindex; j++) {
+				if(heatDRAM[j] < 0) {
+					for(let k = 0; k < DRAMindex - 1; k++) {
+						if(swap[k][0] == -1) {
+							swap[k][0] = ((loc - DRAMindex) % DENSITY_MEM) * UNIT_MEM + X_OFFSET_MEM;
+							swap[k][1] = int((loc - DRAMindex) / DENSITY_MEM) * UNIT_MEM + Y_OFFSET_PMEM;
+							swap[k][2] = (j % DENSITY_MEM) * UNIT_MEM + X_OFFSET_MEM;
+							swap[k][3] = int(j / DENSITY_MEM) * UNIT_MEM + Y_OFFSET_DRAM;
+							swap[k][4] = (swap[k][2] - swap[k][0]) / interval;
+							swap[k][5] = (swap[k][3] - swap[k][1]) / interval;
+
+							swap[k+1][0] = (j % DENSITY_MEM) * UNIT_MEM + X_OFFSET_MEM;
+							swap[k+1][1] = int(j / DENSITY_MEM) * UNIT_MEM + Y_OFFSET_DRAM;
+							swap[k+1][2] = ((loc - DRAMindex) % DENSITY_MEM) * UNIT_MEM + X_OFFSET_MEM;
+							swap[k+1][3] = int((loc - DRAMindex) / DENSITY_MEM) * UNIT_MEM + Y_OFFSET_PMEM;
+							swap[k+1][4] = (swap[k+1][2] - swap[k+1][0]) / interval;
+							swap[k+1][5] = (swap[k+1][3] - swap[k+1][1]) / interval;
+
+							break;
+						}
+					}
+
+					fileList[file].pages[page].location = j;
+					fileList[DRAM[j][0]].pages[DRAM[j][1]] = loc;
+					PMEM[i] = DRAM[j];
+					DRAM[j][0] = file;
+					DRAM[j][1] = page;
+					heatDRAM[j] = 0;
+					break;
+				}
+			}
+
+			heatPMEM[i] = 0;
+		}
+		else if(heatPMEM[i] > -1) heatPMEM[i]--;
+	}
+}
+
+function requestPages() {
+	for(let i = 0; i < 60; i++) {
+
+		let fileIndex = int(random(0, 0.6*fileList.length));
+		let pageIndex = int(random(0, 0.1141 * pow(fileList[fileIndex].size, 2)));
+		fill(255, 255, 255, 128);
+		noStroke();
+		rect(xpos_store(fileList[fileIndex].x + pageIndex % fileList[fileIndex].size),
+				ypos_store(fileList[fileIndex].y + int(pageIndex / fileList[fileIndex].size)),
+				span_store(1), span_store(1));
+
+		if(fileList[fileIndex].pages[pageIndex].location == -1) {
+			if(DRAMindex < HEIGHT_DRAM * DENSITY_MEM) {
+				fileList[fileIndex].pages[pageIndex].location = DRAMindex;
+				DRAM[DRAMindex][0] = fileIndex; DRAM[DRAMindex][1] = pageIndex;
+				heatDRAM[DRAMindex++] = 0;
+			} else if(PMEMindex < HEIGHT_PMEM * DENSITY_MEM) {
+				fileList[fileIndex].pages[pageIndex].location = DRAMindex + PMEMindex;
+				PMEM[PMEMindex][0] = fileIndex; PMEM[PMEMindex][1] = pageIndex;
+				heatPMEM[PMEMindex++] = 0;
+			} else {
+				//search pmem for empty page frame
+			}
+		} else if(fileList[fileIndex].pages[pageIndex].location >= HEIGHT_DRAM * DENSITY_MEM) {
+			heatPMEM[fileList[fileIndex].pages[pageIndex].location - DRAMindex]++;
+		} else {
+			heatDRAM[fileList[fileIndex].pages[pageIndex].location]++;
+		}
+	}
+}
+
+function drawFileStructure() {
+	for (let i = 0; i < DENSITY_STORE; i++) {
+		for (let j = 0; j < DENSITY_STORE; j++) {
+			if (fileStructure[i][j] === -1) continue;
+			rect(xpos_store(j), ypos_store(i), span_store(fileStructure[i][j]), span_store(fileStructure[i][j]));
 		}
 	}
 }
 
 function createFileStructure() {
 	//initilalize
-	const files = create2DArray(DENSITY);
-	for (let i = 0; i < DENSITY; i++)
-		for (let j = 0; j < DENSITY; j++)
+	const files = create2DArray(DENSITY_STORE);
+	for (let i = 0; i < DENSITY_STORE; i++)
+		for (let j = 0; j < DENSITY_STORE; j++)
 			files[i][j] = 0;
 
 	//fill array
-	for (let i = 0; i < DENSITY; i++) {
-		for (let j = 0; j < DENSITY; j++) {
+	for (let i = 0; i < DENSITY_STORE; i++) {
+		for (let j = 0; j < DENSITY_STORE; j++) {
 			if (files[i][j] === -1) continue;
 
 			//find fitting squares
-			let upperBound = DENSITY / 4 + 1;
+			let upperBound = DENSITY_STORE / 4 + 1;
 			let attempt = 0;
 			let found = false;
 			while (!found) {
-				attempt = int(random(1, min(upperBound, DENSITY - max(i, j) + 1)));
+				attempt = int(random(1, min(upperBound, DENSITY_STORE - max(i, j) + 1)));
 
 				let overlap = false;
 				for (let k = 0; k < attempt; k++) {
@@ -103,4 +278,40 @@ function createFileStructure() {
 		}
 	}
 	return files;
+}
+
+function fileAnalysis(fileStructure) {
+	const files = [];
+	let index = 0;
+	for(let i = 0; i < DENSITY_STORE; i++) {
+		for(let j = 0; j < DENSITY_STORE; j++) {
+			if(fileStructure[i][j] == -1) continue;
+			files[index++] = new SimFile(j, i, fileStructure[i][j]);
+		}
+	}
+	return files;
+}
+
+class SimPage {
+	constructor(file, x, y, location) {
+		this.file = file;
+		this.x = x;
+		this.y = y;
+		this.location = location;
+	}
+}
+
+class SimFile {
+	constructor(x, y, size) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		
+		this.pages = [];
+		for(let i = 0; i < this.size; i++) {
+			for(let j = 0; j < this.size; j++) {
+				this.pages[i*this.size + j] = new SimPage(this, j, i, -1);
+			}
+		}
+	}
 }
